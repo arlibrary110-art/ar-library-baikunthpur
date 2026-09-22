15a205d6c29a267347f4f4ad007b67356a13fb07
arlibrary110-art <arlibrary110@gmail.com>
2026-09-22 17:24:46 +0530

    Delete office_qr.php

--- office_qr.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . "/security.php";
-if (!isset($_SESSION["staff_id"])) {
-    header("Location: index.php");
-    exit;
-}
-header("Location: qr_display.php", true, 302);
-exit;
